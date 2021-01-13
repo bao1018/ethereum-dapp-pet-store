d3eb77b3e45e5ba7a2117a677d1d93e2079be4b2
Jeremy B <bao1018@gmail.com>
2021-01-13 16:44:25 +0800

    small change to make it work in china network

--- truffle.js
+++ truffle.js
@@ -1,3 +1,6 @@
+const PrivateKeyProvider = require("@truffle/hdwallet-provider");
+const privateKey = "8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63";
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -6,6 +9,10 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
-    }
+    },
+    besu: {
+      provider: () => new PrivateKeyProvider(privateKey, "http://localhost:8545"),
+      network_id: "*"
+    }, 
   }
 };
